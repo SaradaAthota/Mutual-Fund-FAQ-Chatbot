e5705a51f8e27850ebb3f854eda8060036f4797d
Sarada <sarada.praneeth@gmail.com>
2025-11-17 20:16:04 +0530

    Updated CORS script in app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -48,6 +48,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "http://127.0.0.1:5174",
+        "http://localhost:5174",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
         "https://mf-faq-backend-production.up.railway.app",
